8bb3fbb3e277325b9d2ddcabb92ebc92f2274d44
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-10-01 14:40:21 +0100

    enhancement: made landing page and image page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-
+import {useState, useEffect} from 'react';
+import {  Routes, Route, BrowserRouter } from 'react-router-dom';
+import LandingPage from './pages/landingpage/LandingPage';
+import ImagePage from './pages/imagespage/ImagesPage';
 import './App.css'
 
 function App() {
@@ -6,7 +9,17 @@ function App() {
 
   return (
     <>
-      <p>hello world</p>
+      <BrowserRouter>
+     <div>
+    
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/ImagePage" element = {<ImagePage/>} />
+        
+     
+      </Routes>
+      </div>
+    </BrowserRouter>
     </>
   )
 }
