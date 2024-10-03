16525907f5daf9df5bba52cbacbaa294e877b623
IanUpton <uptonian22@gmail.com>
2024-10-03 12:36:17 +0100

    enhancement: repaired under tutors direction

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,15 @@ import './App.css'
 import Layout from './components/layout/Layout';
 
 function App() {
-
   const [loggedUser, setLoggedUser] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-
+  
   return (
     <>    
      <div>
         <Layout>
           <Routes>
-            <Route path="/" element={<LandingPage setLoggedUser={setLoggedUser} setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/" element={<LandingPage setLoggedUser={setLoggedUser} setIsLoggedIn={setIsLoggedIn}/>} />
             <Route path="/images" element = {<ImagesPage/>} />
           </Routes>
          </Layout>
